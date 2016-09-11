b154543d631b04af8a34731046536a6842925feb
grigoryMovchan <grigory08@ya.ru>
2016-09-11 17:01:27 +0300

    Pull

--- list.php
+++ list.php
@@ -26,28 +26,8 @@
 
   $queryList = "SELECT `id`, `from`, `description`, `target`, `contacts`, `timestamp` FROM `soc` WHERE `city`='$city' AND `gender`='$gender' AND `targetGender`='$targetGender' ORDER BY `timestamp` DESC";
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Soc</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+  require_once("header.php");
+  ?>
     <br>
 
     <div class="container">
@@ -153,9 +133,6 @@
       ?>
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php
+  require_once("footer.php");
+?>
